af0f8116200f64182df43843bec81b44a0d336a8
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-06-08 23:50:14 +0530

    Ability To Add BST

--- src/test/java/com/magic/bst/MyBinaryTreeTest.java
+++ src/test/java/com/magic/bst/MyBinaryTreeTest.java
@@ -2,6 +2,7 @@ package com.magic.bst;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.annotation.Testable;
 
 public class MyBinaryTreeTest {
     @Test
@@ -12,6 +13,25 @@ public class MyBinaryTreeTest {
         myBinaryTree.add(70);
         int size = myBinaryTree.getSize();
         Assertions.assertEquals(3,size);
+    }
+    @Test
+    public void newElements(){
+        MyBinaryTree <Integer> myBinaryTree = new MyBinaryTree();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        myBinaryTree.add(95);
+        myBinaryTree.add(67);
+        myBinaryTree.add(63);
+        myBinaryTree.add(65);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+        myBinaryTree.add(11);
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(60);
+        int size = myBinaryTree.getSize();
+        Assertions.assertEquals(13,size);
     }
 
 }
